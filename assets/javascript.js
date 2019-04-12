def314428e64bcf8294574c12302d95f0e8dd9c4
RickN123 <nicolai.richard@gmail.com>
2019-04-11 21:24:46 -0400

    base

---
+++ assets/javascript.js
@@ -0,0 +1,26 @@
+var config = {
+    apiKey: "AIzaSyDpcxMdkuAP0gHULhySRyqIXb9QdFQRfeY",
+    authDomain: "traincalendar-8d29e.firebaseapp.com",
+    databaseURL: "https://traincalendar-8d29e.firebaseio.com",
+    projectId: "traincalendar-8d29e",
+    storageBucket: "traincalendar-8d29e.appspot.com",
+    messagingSenderId: "324240328259"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+$(document.readyState(function () {
+
+    $("submit").on("click", function (event) {
+
+        event.preventDefault();
+        console.log(this):
+
+        var newTrain = {
+
+
+        }
+    }
+  }
+
